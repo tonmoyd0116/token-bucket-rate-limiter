8e9903d1533523839e54e739ea1dcc1d8f8bce19
tonmoyd0116 <tonmoydey598@gmail.com>
2026-07-16 21:29:30 +0530

    Logger Implementation was made

--- src/main/java/com/tonmoyd0116/ratelimiter/controller/HealthController.java
+++ src/main/java/com/tonmoyd0116/ratelimiter/controller/HealthController.java
@@ -2,16 +2,21 @@ package com.tonmoyd0116.ratelimiter.controller;
 
 import com.tonmoyd0116.ratelimiter.dto.ApiResponse;
 import com.tonmoyd0116.ratelimiter.dto.HealthResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
 
+@Slf4j
 @RestController
-public class RateLimiterController {
-
-    @GetMapping("/api/health")
+@RequestMapping("/api/v1")
+public class HealthController {
+    @GetMapping("/health")
     public ResponseEntity<ApiResponse<HealthResponse>> healthCheck(){
         HealthResponse healthResponse = new HealthResponse("UP");
         ApiResponse<HealthResponse> response =
@@ -22,8 +27,7 @@ public class RateLimiterController {
                         Instant.now(),
                         healthResponse
                 );
-
+        log.info("Health Check API hit was made at time {}",Instant.now());
         return ResponseEntity.ok(response);
     }
-
 }
